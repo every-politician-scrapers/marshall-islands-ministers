08eabe9e90c0e5a76aa303ad4a9e651426bd10d9
Tony Bowden <tony@tmtm.com>
2021-12-10 10:00:58 +0200

    Don't report on plain Senators
    This is a list of all members of the legislature, which also includes
    their executive positions. We may as well gather them all, but we only
    want to report on the ones that hold other positions.

--- bin/diff.rb
+++ bin/diff.rb
@@ -4,4 +4,5 @@
 require 'every_politician_scraper/comparison'
 
 diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+                                         .reject { |row| row.last == 'Senator' }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
